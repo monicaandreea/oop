c9069acfe8e44c32e2744bcf6661c9c6912e49ce
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2021-03-20 18:31:39 +0200

    Update Caiet.h

--- Caiet.h
+++ Caiet.h
@@ -1,3 +1,6 @@
+#ifndef LIBRARIE_CAIET_H
+#define LIBRARIE_CAIET_H
+
 #include<iostream>
 
 class Caiet{
@@ -7,11 +10,16 @@ private:
     int stock;
     int price;
 public:
-    Caiet(const int &pag, std::string tip, int stoc, int price);
+    Caiet(const int &pag, std::string tip, int stoc, int pret);
     int getPages();
     std::string getType();
+    void setStock(int s);
     int getStock();
-    Caiet &operator=(const Caiet &c);
     friend std::ostream &operator<<( std::ostream &output, const Caiet &c );
-
+    friend bool operator==( const Caiet &c1, const Caiet &c2);
+    Caiet(const Caiet &c);
+    Caiet& operator= (const Caiet &c);
+    friend bool operator<( const Caiet &c1, const Caiet &c2);
+    friend bool operator>( const Caiet &c1, const Caiet &c2);
 };
+#endif //LIBRARIE_CAIET_H
