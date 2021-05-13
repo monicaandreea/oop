6fa253569f4256caccd337a9913241eb5c8e0d28
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2021-05-13 11:07:06 +0300

    Update ManualLiceu.cpp

--- ManualLiceu.cpp
+++ ManualLiceu.cpp
@@ -1,14 +1,22 @@
 
 #include "ManualLiceu.h"
 
-ManualLiceu::ManualLiceu(const int &cls, std::string sub, int stoc, int pret, std::string prof)
-        : Manual(cls, sub, stoc, pret), profil(prof){}
 
 std::string ManualLiceu::getProfil() {
     return profil;
 }
 
-/*int ManualLiceu::getPrice() {
-    return Manual::getPrice() + 100;
-} */
+int ManualLiceu::getPrice() {
+    return Manual::getPrice() + 10;
+}
 
+ManualLiceu::ManualLiceu(const int &cls, std::string sub, int stoc, int pret)
+        : Manual(cls, sub, stoc, pret){}
+
+
+std::unique_ptr <Manual> ManualLiceu::clone(){
+    return std::make_unique <ManualLiceu>(*this);
+}
+
+ManualLiceu::ManualLiceu(const int &cls, std::string sub, int stoc, int pret, std::string prof)
+        : Manual(cls, sub, stoc, pret), profil(prof){}
