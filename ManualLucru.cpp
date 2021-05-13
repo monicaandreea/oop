958774917df9308de348367fafdbd163db81c50d
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2021-05-13 11:06:21 +0300

    Update ManualLucru.cpp

--- ManualLucru.cpp
+++ ManualLucru.cpp
@@ -1,9 +1,6 @@
 
 #include "ManualLucru.h"
 
-ManualLucru::ManualLucru(const int &cls, std::string sub, int stoc, int pret, int dif, int rez)
-    : Manual(cls, sub, stoc, pret), dificultate(dif), rezolvari(rez){}
-
 bool ManualLucru::getRez() {
     return rezolvari;
 }
@@ -11,3 +8,18 @@ bool ManualLucru::getRez() {
 int ManualLucru::getDificulty() {
     return dificultate;
 }
+
+int ManualLucru::getPrice() {
+    return Manual::getPrice() -5; //reducere?
+}
+
+ManualLucru::ManualLucru(const int &cls, std::string sub, int stoc, int pret)
+        : Manual(cls, sub, stoc, pret){}
+
+
+std::unique_ptr <Manual> ManualLucru::clone(){
+    return std::make_unique <ManualLucru>(*this);
+}
+
+ManualLucru::ManualLucru(const int &cls, std::string sub, int stoc, int pret, int dif, int rez)
+        : Manual(cls, sub, stoc, pret), dificultate(dif), rezolvari(rez){}
