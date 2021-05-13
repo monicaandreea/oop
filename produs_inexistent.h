5e7f4be38f3d96a2cf02403b20c6adfc6f5eb283
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2021-05-13 11:04:33 +0300

    Update produs_inexistent.h

--- produs_inexistent.h
+++ produs_inexistent.h
@@ -4,11 +4,14 @@
 #include <stdexcept>
 #include "Caiet.h"
 #include "Manual.h"
+#include "ManualLiceu.h"
+#include "ManualLucru.h"
 
 class produs_inexistent : public std::runtime_error {
 public:
     produs_inexistent(Caiet prod) noexcept;
-    produs_inexistent(Manual prod) noexcept;
+    produs_inexistent(ManualLiceu prod) noexcept;
+    produs_inexistent(ManualLucru prod) noexcept;
 };
 
 #endif //LIBRARIE_PRODUS_INEXISTENT_H
