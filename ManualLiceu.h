4e0a4562933ede94be47ee70cd9dd2ef37147021
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2021-05-13 11:06:43 +0300

    Update ManualLiceu.h

--- ManualLiceu.h
+++ ManualLiceu.h
@@ -2,14 +2,17 @@
 #define LIBRARIE_MANUALLICEU_H
 
 #include "Manual.h"
+#include <memory>
 
 class ManualLiceu : public Manual{
 private:
     std::string profil;
 public:
+    ManualLiceu(const int &cls, std::string sub, int stoc, int pret);
     ManualLiceu(const int &cls, std::string sub, int stoc, int pret, std::string prof);
     std::string getProfil();
-    //int getPrice() override;
+    int getPrice() override;
+    std::unique_ptr <Manual> clone() override;
 };
 
 #endif //LIBRARIE_MANUALLICEU_H
