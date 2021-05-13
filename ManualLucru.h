86fe45b237488a67c2620bff48df1715f54c58fd
Gîrbea Monica <79595825+monicaandreea@users.noreply.github.com>
2021-05-13 11:05:56 +0300

    Update ManualLucru.h

--- ManualLucru.h
+++ ManualLucru.h
@@ -8,9 +8,12 @@ private:
     int dificultate; //1- avansat, 2-mediu, 3-incepator
     bool rezolvari; //0-nu are rezolvari, 1-are rezolvari
 public:
+    ManualLucru(const int &cls, std::string sub, int stoc, int pret);
     ManualLucru(const int &cls, std::string sub, int stoc, int pret, int dif, int rez);
     int getDificulty();
     bool getRez();
+    int getPrice() override;
+    std::unique_ptr <Manual> clone() override;
 };
 
 #endif //LIBRARIE_MANUALLUCRU_H
